036846470ea95b6489a8cde128260643df766a3c
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-09-30 23:18:06 +0300

    complete all tasks

--- resources/sql/sql-task.sql
+++ resources/sql/sql-task.sql
@@ -41,3 +41,89 @@ select airport_name ->> 'en' airport_name, count(f.flight_id)
 from airports_data ad 
 inner join flights f on airport_code = f.departure_airport 
 group by airport_code
+--Task8
+select flight_id
+from flights f 
+where actual_arrival is not null and extract(epoch from (actual_arrival - scheduled_arrival)) > 0
+--Task9
+select ad.aircraft_code, ad.model ->> 'ru' model, s.seat_no 
+from seats s
+inner join aircrafts_data ad using (aircraft_code)
+where  s.fare_conditions = 'Economy' and ad.model ->> 'ru' = 'Аэробус A321-200'
+order by s.seat_no
+--Task10
+select ad.airport_code, ad.airport_name ->> 'en' as airport, ad.city ->> 'en' as city
+from airports_data ad
+where ad.city ->> 'en' in (select city ->> 'en' as city
+from airports_data ad 
+group by (ad.city ->> 'en')
+having count(airport_code) > 1)
+--Task11
+select t.passenger_id, b.total_amount
+from tickets t 
+left join bookings b using(book_ref)
+inner join (select book_ref, count (passenger_id) numbers
+from tickets t 
+left join bookings b using(book_ref)
+group by book_ref) as qi on t.book_ref = qi.book_ref
+where b.total_amount > (select avg(total_amount) from bookings b)
+--Task12
+select f.flight_id , adfrom.airport_name, adto.airport_name 
+from flights f 
+inner join airports_data adfrom on f.departure_airport =adfrom.airport_code 
+inner join airports_data adto on f.arrival_airport =adto.airport_code 
+where f.status = 'On Time' 
+and adfrom.city ->> 'ru' = 'Екатеринбург'
+and adto.city ->> 'ru' = 'Москва'
+--Task13
+(select tick, costs
+from (select t.ticket_no as tick, b.total_amount / bttemp.counts as costs
+from tickets t 
+inner join bookings b using(book_ref) 
+inner join (select book_ref, count(ticket_no) as counts
+from tickets tb 
+group by book_ref ) as bttemp using(book_ref)
+order by costs
+) as minmaxtable
+limit 1)
+union all 
+(select tick, costs
+from (select t.ticket_no as tick, b.total_amount / bttemp.counts as costs
+from tickets t 
+inner join bookings b using(book_ref) 
+inner join (select book_ref, count(ticket_no) as counts
+from tickets tb 
+group by book_ref ) as bttemp using(book_ref)
+order by costs desc 
+) as minmaxtable
+limit 1)
+--Task14
+create table public.customers(
+customer_id bigserial PRIMARY KEY,
+firstname varchar  not null,
+lastname varchar  not null,
+email varchar  unique, 
+phone varchar  unique not null
+)
+--Task15
+create table public.orders(
+order_id bigserial primary key ,
+customer_id bigint references public.customers (customer_id),
+product_name varchar  not null,
+price decimal(100,2)  not null,
+quantity int  not null, 
+sum  decimal(100,2) not null
+)
+--Task16
+insert into public.customers 
+(firstname, lastname, email, phone)
+values ('Сидр', 'Сидоров', 'wqer@mail.ru', '+375291285697'), ('Петр', 'Петров', 'Petrovich@yandex.ru', '375291597536'),
+('Иван', 'Иванович', 'jan@yandex.ru', '375448527411'), ('Гена', 'Генадьев', 'gen@mail.ru', '375297896541'),
+('Ольга', 'Петровна', 'Olja@gmail.ru', '375335648532');
+insert into public.orders 
+(customer_id, product_name, price, quantity, sum) 
+values (1, 'iphone16', 1000, 1, 1000), (2, 'macbook', 3000, 2, 6000),
+(3, 'vacuumrobot', 500, 3, 1500), (4, 'yandexstation', 500, 2, 1000),
+(5, 'kettle', 50, 10, 500)
+--Task17
+drop table orders, customers;
